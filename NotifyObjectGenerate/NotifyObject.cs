bf8ab7a59e14abfeddf8b74d7044a5ae9812f15e
hongy <hongy19890331@outlook.com>
2014-01-04 12:30:50 +0800

    NotifyObject

---
+++ NotifyObjectGenerate/NotifyObject.cs
@@ -0,0 +1,64 @@
+﻿namespace NotifyObjectGenerate.Model
+{
+	using System;
+	using System.ComponentModel;
+	using System.Xml;
+
+	public class TestA : INotifyPropertyChanged
+	{
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		private int _Id;
+		public int Id
+		{
+			get { return _Id; }
+			set 
+			{
+				_Id = value;
+				if(PropertyChanged != null)
+					PropertyChanged(this, new PropertyChangedEventArgs("Id"));
+			}
+		}
+
+		private string _Name;
+		public string Name
+		{
+			get { return _Name; }
+			set 
+			{
+				_Name = value;
+				if(PropertyChanged != null)
+					PropertyChanged(this, new PropertyChangedEventArgs("Name"));
+			}
+		}
+	}
+
+	public class TestB : INotifyPropertyChanged
+	{
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		private int _Id;
+		public int Id
+		{
+			get { return _Id; }
+			set 
+			{
+				_Id = value;
+				if(PropertyChanged != null)
+					PropertyChanged(this, new PropertyChangedEventArgs("Id"));
+			}
+		}
+
+		private string _Name;
+		public string Name
+		{
+			get { return _Name; }
+			set 
+			{
+				_Name = value;
+				if(PropertyChanged != null)
+					PropertyChanged(this, new PropertyChangedEventArgs("Name"));
+			}
+		}
+	}
+}
---
+++ NotifyObjectGenerate/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace NotifyObjectGenerate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var xml = XElement.Load("Object.xml");
+            Console.WriteLine(xml.Element("using").Value);
+        }
+    }
+}
